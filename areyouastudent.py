c17ed2df83bb124c8713d854bdd1c20712a1417f
Kat Camp <kcamp552369@gmail.com>
2022-04-27 16:51:35 -0400

    setup

---
+++ areyouastudent.py
@@ -0,0 +1,34 @@
+
+SCHOOL_NAME = "Career devs"
+
+STUDENTS = ["Kat", "Nick", "Bill", "Keith", "Jazlynn", "coralona", "John", "Manny"]
+
+# Welcome message + student list  listed out
+print("HELLO CAREER DEVS!!!!! ")
+print("\nWelcome to the " + SCHOOL_NAME + " student check-in\n")
+for student_name in STUDENTS:
+     print("-- " + student_name)
+print("\n")
+
+
+# user input section (buggy code)
+def checker():
+    accept = input("Do you attend " + SCHOOL_NAME + "?\n(yes/no): ").strip()
+    if accept[0] == "y":
+        name = input("What is your name?\nName: ").strip()
+        for student_name in STUDENTS:
+            if name.casefold() == student_name.casefold():
+                print("Thank you " + student_name + ", you are now checked in.")
+                exit()
+        print("You're not a current student ")
+        exit()
+    if accept[0] == "n":
+        print("This service is not for you. Exiting program...")
+        exit()
+    if accept[0] != "y" or "n":
+        print("Please enter a valid response")
+        checker()
+
+
+checker()
+exit()
--- employee_checker.py
+++ employee_checker.py
@@ -4,39 +4,36 @@
 # allow a user to re-enter if they dont enter yes or no
 
 
-
-
 COMPANY_NAME = "Tech Inc."
 
 EMPLOYEES = ["Alice", "Bob", "Cameron", "Dan", "Ellen", "Frank", "Grace", "Harry"]
 
 # Welcome message + employee names listed out
-print("\nWelcome to the "+COMPANY_NAME+" employee check-in\n")
+print("\nWelcome to the " + COMPANY_NAME + " employee check-in\n")
 print("We at " + COMPANY_NAME + " are very proud of our employees: ")
 for emp_name in EMPLOYEES:
-    print("-- "+emp_name)
+    print("-- " + emp_name)
 print("\n")
-
 
 
 # user input section (buggy code)
 def checker():
-    accept = input("Do you work for " + COMPANY_NAME + "?\n(yes/no): ")
+    accept = input("Do you work for " + COMPANY_NAME + "?\n(yes/no): ").strip()
     if accept[0] == "y":
         name = input("What is your name?\nName: ").strip()
         for emp_name in EMPLOYEES:
-         if name.casefold() == emp_name.casefold():
-            print("Thank you " + emp_name + ", you are now checked in.")
-            exit()
-            if name.casefold() != emp_name.casefold():
-                print("You're not an employee")
+            if name.casefold() == emp_name.casefold():
+                print("Thank you " + emp_name + ", you are now checked in.")
                 exit()
-
+        print("You're not an employee")
+        exit()
     if accept[0] == "n":
         print("This service is not for you. Exiting program...")
         exit()
     if accept[0] != "y" or "n":
         print("Please enter a valid response")
         checker()
+
+
 checker()
 exit()
